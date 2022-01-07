cdf1100852c7ff47d19324814e75810695dbba84
Glend Maatita <glend@landx.id>
2022-01-07 12:57:53 +0700

    add workflow sync git

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ mkdir -p /root/.ssh
 echo "$INPUT_SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 
+git fetch --unshallow
 git remote add destination "$INPUT_TARGET_REPO_URL"
 git push destination "$INPUT_SOURCE_BRANCH:$INPUT_DESTINATION_BRANCH" -f
